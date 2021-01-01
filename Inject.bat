dabc42bd0bf34b9d8d883c41881ef74019640028
Matt <33806410+Mattz-P@users.noreply.github.com>
2021-01-01 04:31:31 -0800

    Unzipped

--- Inject.bat
+++ Inject.bat
@@ -3,6 +3,7 @@ xcopy KAN "C:\Users\matth\AppData\Local\Forge\Cache\pics\cards\KAN" /I /Y
 xcopy D:\m\File\Cards\Aimport\res "D:\m\Games\Forge 36\res" /I /Y /S
 
 REM Compile Cards
+
 cd "D:\m\Games\Forge 36\res\cardsfolder"
 start cmd.exe /C "D:\m\Games\Forge 36\res\cardsfolder\ZIP.bat"
 
